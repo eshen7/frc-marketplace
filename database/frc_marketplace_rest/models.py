b497a3b6e3fd0171b48ba034359beae9b798a017
acheney <andrewkkchen@gmail.com>
2024-10-21 00:39:25 -0500

    prune frontend, initialize rest framework

---
+++ database/frc_marketplace_rest/models.py
@@ -0,0 +1,31 @@
+# PART REQUESTS
+# Title
+# Photos
+# Part name
+# Model specs
+# Need by
+# Need it for what
+# Extra info
+# Condition of part
+# Team requesting
+
+import uuid
+
+from django.db import models
+from phone_field import PhoneField
+from address.models import AddressField
+
+class PartRequest(models.Model):
+    pass
+
+class User(models.Model):
+    UUID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    team_name = models.CharField()
+    team_number = models.IntegerField(unique=True)
+    phone = PhoneField(unique=True)
+    address = AddressField()
+    password = models.CharField(max_length=128) # store hashed password
+
+    def set_password(self, raw_password):
+        """Hashes password and stores it"""
