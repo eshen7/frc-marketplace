0bcb5a0eab2b22832e8487dcc883238964077581
acheney <andrewkkchen@gmail.com>
2024-10-23 02:25:54 -0500

    updated .env, added migration script to docker compose, created user admin model

--- frc_marketplace_database/api/admin.py
+++ frc_marketplace_database/api/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .models import User
 
 # Register your models here.
+
+
+class UserAdmin(BaseUserAdmin):
+    """display configuration for User object"""
+    list_display = ('email', 'team_name', 'team_number', 'is_active', 'is_staff', 'date_joined')
+    list_filter = ('is_active', 'is_staff', 'date_joined', 'team_name')
+    search_fields = ('email', 'team_name', 'team_number')
+    ordering = ('-date_joined',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('team_name', 'team_number', 'phone', 'address')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'team_name', 'team_number', 'is_active', 'is_staff'),
+        }),
+    )
+
+admin.site.register(User, UserAdmin)
+
--- frc_marketplace_database/api/models.py
+++ frc_marketplace_database/api/models.py
@@ -12,6 +12,7 @@
 import uuid
 
 from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 from phone_field import PhoneField
 from address.models import AddressField
@@ -21,7 +22,7 @@ class UserManager(BaseUserManager):
     """user manager for each account"""
 
     def create_user(self, email, password=None, **extra_fields):
-        """protocol for creating users"""
+        """protocol to create regular users"""
         if not email:
             raise ValueError("Email is required!")
         email = self.normalize_email(email)
@@ -30,27 +31,41 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+    def create_superuser(self, email, password, **extra_fields):
+        """protocol to create superusers (admin)"""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
 
-class User(AbstractBaseUser):
+        if not extra_fields.get('is_staff'):
+            raise ValueError('Superuser is_staff field is required!')
+        if not extra_fields.get('is_superuser'):
+            raise ValueError('Superuser is_superuser field is required!')
+
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser,PermissionsMixin):
     """user model"""
     UUID = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
     team_name = models.CharField()
     team_number = models.IntegerField(unique=True)
     phone = PhoneField(unique=True)
-    address = AddressField()
+    address = AddressField(null=True, blank=True)
     password = models.CharField(max_length=128)  # store hashed password
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False) # base user is not staff by default
+    is_superuser = models.BooleanField(default=False) # base user is not superuser by default
     date_joined = models.DateTimeField(auto_now_add=True)
 
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ["team_name", "team_number","phone"]
+    REQUIRED_FIELDS = ["team_name", "team_number","phone","address"]
 
     def __str__(self):
-        return self.UUID
+        return self.email
 
     def set_password(self, raw_password):
         """hashes password and stores it"""
         super().set_password(raw_password=raw_password)
+
