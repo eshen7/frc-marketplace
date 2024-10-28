e75c4c371827e845d77fef4f272961194653faef
acheney <andrewkkchen@gmail.com>
2024-10-28 01:29:08 -0500

    fixed signup components

--- vite-frontend/src/pages/Signup.jsx
+++ vite-frontend/src/pages/Signup.jsx
@@ -3,9 +3,7 @@ import TopBar from "./../components/TopBar.jsx";
 import { Box, Button } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import axiosInstance from "../utils/axiosInstance.js";
-
-const handleChange = (e) => {};
-import Footer from './../components/Footer.jsx'
+import Footer from "./../components/Footer.jsx";
 
 const Signup = () => {
   // States here have to be formatted according to the backend Model schema
@@ -63,19 +61,114 @@ const Signup = () => {
   return (
     <>
       <TopBar />
-      <div className='flex justify-center bg-gray-100'>
-        <div className='flex flex-col bg-white min-h-fit container my-20 justify-center w-2/3 mx-auto py-20 rounded-3xl min-w-96 shadow-xl'>
-          <h1 className='text-5xl text-red-800 font-bold font-roboto text-center mb-[100px]'>Sign up</h1>
-          <div className='flex flex-col gap-5'>
-
-            <div className="w-2/3 mx-auto"><TextField id="outlined-basic" required label="Head Mentor Full Name" variant="outlined" className='w-full' /></div>
-            <div className="w-2/3 mx-auto"><TextField id="outlined-basic" required type="number" label="Team Number" variant="outlined" className='w-full' /></div>
-            <div className="w-2/3 mx-auto"><TextField id="outlined-basic" required label="Address" variant="outlined" className='w-full' /></div>
-            <div className="w-2/3 mx-auto"><TextField id="outlined-basic" required label="Email" variant="outlined" className='w-full' /></div>
-            <div className="w-2/3 mx-auto"><TextField id="outlined-basic" required label="Phone Number" variant="outlined" className='w-full' /></div>
-            <div className="w-2/3 mx-auto"><TextField id="outlined-basic" required label="Password" variant="outlined" className='w-full' /></div>
-            <div className="w-2/3 mx-auto"><TextField id="outlined-basic" required label="Re-type Password" variant="outlined" className='w-full' /></div>
-            <div className="w-2/3 mx-auto"><TextField id="outlined-basic" required label="Confirmation Code" variant="outlined" className='w-full' /></div>
+      <div className="flex justify-center bg-gray-100">
+        <div className="flex flex-col bg-white min-h-fit container my-20 justify-center w-2/3 mx-auto py-20 rounded-3xl min-w-96 shadow-xl">
+          <h1 className="text-5xl text-red-800 font-bold font-roboto text-center mb-[100px]">
+            Sign up
+          </h1>
+          <div className="flex flex-col gap-5">
+            <div className="w-2/3 mx-auto">
+              <TextField
+                id="outlined-basic"
+                required
+                label="Head Mentor Full Name"
+                variant="outlined"
+                className="w-full"
+                value={formData.full_name}
+                onChange={(e) => handleChange("full_name", e.target.value)}
+              />
+            </div>
+            <div className="w-2/3 mx-auto">
+              <TextField
+                id="outlined-basic"
+                required
+                type="number"
+                label="Team Number"
+                variant="outlined"
+                className="w-full"
+                value={formData.team_number}
+                onChange={(e) => handleChange("team_number", e.target.value)}
+              />
+            </div>
+            <div className="w-2/3 mx-auto">
+              <TextField
+                id="outlined-basic"
+                required
+                label="Address"
+                variant="outlined"
+                className="w-full"
+                value={formData.address}
+                onChange={(e) => handleChange("address", e.target.value)}
+              />
+            </div>
+            <div className="w-2/3 mx-auto">
+              <TextField
+                id="outlined-basic"
+                required
+                label="Email"
+                type="email"
+                variant="outlined"
+                className="w-full"
+                value={formData.email}
+                onChange={(e) => handleChange("email", e.target.value)}
+              />
+            </div>
+            <div className="w-2/3 mx-auto">
+              <TextField
+                id="outlined-basic"
+                required
+                label="Phone Number"
+                variant="outlined"
+                className="w-full"
+                value={formData.phone}
+                onChange={(e) => handleChange("phone", e.target.value)}
+              />
+            </div>
+            <div className="w-2/3 mx-auto">
+              <TextField
+                id="outlined-basic"
+                required
+                label="Password"
+                variant="outlined"
+                className="w-full"
+                type="password"
+                autoComplete="current-password"
+                value={formData.password}
+                onChange={(e) => handleChange("password", e.target.value)}
+                error={!!passwordError}
+                helperText={passwordError}
+              />
+            </div>
+            <div className="w-2/3 mx-auto">
+              <TextField
+                id="outlined-basic"
+                required
+                label="Re-type Password"
+                variant="outlined"
+                className="w-full"
+                type="password"
+                autoComplete="current-password"
+                value={formData.passwordConfirmation}
+                onChange={(e) =>
+                  handleChange("passwordConfirmation", e.target.value)
+                }
+                error={!!passwordError}
+                helperText={passwordError}
+              />
+            </div>
+            <div className="w-2/3 mx-auto">
+              <TextField
+                id="outlined-basic"
+                required
+                label="Confirmation Code"
+                variant="outlined"
+                className="w-full"
+                value={formData.confirmationCode}
+                onChange={(e) =>
+                  handleChange("confirmationCode", e.target.value)
+                }
+              />
+            </div>
 
             <div className="flex flex-row mx-10 justify-center">
               <div className="w-1/2 flex justify-center">
@@ -87,9 +180,8 @@ const Signup = () => {
                 <Button
                   variant="contained"
                   className="justify-end w-1/2 whitespace-nowrap"
-                  onClick={() => {
-                    handleSubmit();
-                  }}
+                  onClick={handleSubmit}
+                  disabled={!!passwordError}
                 >
                   Sign up
                 </Button>
