68597e07acc2bf4a70fecfd988808c1502d280dc
ericg4 <eric.g4@outlook.com>
2024-11-07 10:48:53 -0600

    fixed autofill for signup page

--- vite-frontend/src/pages/Signup.jsx
+++ vite-frontend/src/pages/Signup.jsx
@@ -113,132 +113,136 @@ const Signup = () => {
             Sign up
           </h1>
           <div className="flex flex-col gap-5">
-            <div className="w-2/3 mx-auto">
-              <TextField
-                id="outlined-basic"
-                required
-                label="Head Mentor Full Name"
-                variant="outlined"
-                className="w-full"
-                value={formData.full_name}
-                onChange={(e) => handleChange("full_name", e.target.value)}
-              />
-            </div>
-            <div className="w-2/3 mx-auto">
-              <TextField
-                id="outlined-basic"
-                required
-                type="number"
-                label="Team Number"
-                variant="outlined"
-                className="w-full"
-                value={formData.team_number}
-                onChange={(e) => handleChange("team_number", e.target.value)}
-              />
-            </div>
-            <div className="w-2/3 mx-auto">
-              <TextField
-                id="address-input" // Important: This ID is used by Google Places
-                required
-                label="Address"
-                variant="outlined"
-                className="w-full"
-                value={formData.address}
-                onChange={(e) => handleChange("address", e.target.value)}
-                placeholder="Start typing your address..."
-              />
-            </div>
-            <div className="w-2/3 mx-auto">
-              <TextField
-                id="outlined-basic"
-                required
-                label="Email"
-                type="email"
-                variant="outlined"
-                className="w-full"
-                value={formData.email}
-
-                onChange={(e) => handleChange("email", e.target.value)}
-              />
-            </div>
-            <div className="w-2/3 mx-auto">
-              <TextField
-                id="outlined-basic"
-                required
-                label="Phone Number"
-                type="number"
-                variant="outlined"
-                className="w-full"
-                value={formData.phone}
-                onChange={(e) => handleChange("phone", e.target.value)}
-              />
-            </div>
-            <div className="w-2/3 mx-auto">
-              <TextField
-                id="outlined-basic"
-                required
-                label="Password"
-                variant="outlined"
-                className="w-full"
-                type="password"
-                autoComplete="current-password"
-                value={formData.password}
-                onChange={(e) => handleChange("password", e.target.value)}
-                error={!!passwordError}
-                helperText={passwordError}
-              />
-            </div>
-            <div className="w-2/3 mx-auto">
-              <TextField
-                id="outlined-basic"
-                required
-                label="Re-type Password"
-                variant="outlined"
-                className="w-full"
-                type="password"
-                autoComplete="current-password"
-                value={formData.passwordConfirmation}
-                onChange={(e) =>
-                  handleChange("passwordConfirmation", e.target.value)
-                }
-                error={!!passwordError}
-                helperText={passwordError}
-              />
-            </div>
-            <div className="w-2/3 mx-auto">
-              <TextField
-                id="outlined-basic"
-                required
-                label="Confirmation Code"
-                variant="outlined"
-                className="w-full"
-                value={formData.confirmationCode}
-                onChange={(e) =>
-                  handleChange("confirmationCode", e.target.value)
-                }
-              />
-            </div>
-
-            <div className="flex flex-row mx-10 justify-center">
-              <div className="w-1/2 flex justify-center">
-                <a href="login" className="text-blue-600 hover:text-blue-800">
-                  Sign In Instead
-                </a>
+              <div className="w-2/3 mx-auto">
+                <TextField
+                  id="outlined-basic"
+                  required
+                  name="name"
+                  autoComplete="name"
+                  type="text"
+                  label="Head Mentor Full Name"
+                  variant="outlined"
+                  className="w-full"
+                  value={formData.full_name}
+                  onChange={(e) => handleChange("full_name", e.target.value)}
+                />
               </div>
-              <div className="w-1/2 flex justify-center">
-                <Button
-                  variant="contained"
-                  className="justify-end w-1/2 whitespace-nowrap"
-                  onClick={handleSubmit}
-                  disabled={!!passwordError}
-                >
-                  Sign up
-                </Button>
+              <div className="w-2/3 mx-auto">
+                <TextField
+                  id="outlined-basic"
+                  required
+                  type="number"
+                  label="Team Number"
+                  variant="outlined"
+                  className="w-full"
+                  value={formData.team_number}
+                  onChange={(e) => handleChange("team_number", e.target.value)}
+                />
               </div>
-            </div>
+              <div className="w-2/3 mx-auto">
+                <TextField
+                  id="address-input" // Important: This ID is used by Google Places
+                  required
+                  label="Address"
+                  variant="outlined"
+                  className="w-full"
+                  value={formData.address}
+                  onChange={(e) => handleChange("address", e.target.value)}
+                  placeholder="Start typing your address..."
+                />
+              </div>
+              <div className="w-2/3 mx-auto">
+                <TextField
+                  id="outlined-basic"
+                  required
+                  label="Email"
+                  type="email"
+                  variant="outlined"
+                  className="w-full"
+                  value={formData.email}
+
+                  onChange={(e) => handleChange("email", e.target.value)}
+                />
+              </div>
+              <div className="w-2/3 mx-auto">
+                <TextField
+                  id="outlined-basic"
+                  required
+                  label="Phone Number"
+                  type="number"
+                  variant="outlined"
+                  className="w-full"
+                  value={formData.phone}
+                  onChange={(e) => handleChange("phone", e.target.value)}
+                />
+              </div>
+              <div className="w-2/3 mx-auto">
+                <TextField
+                  id="outlined-basic"
+                  required
+                  label="Password"
+                  variant="outlined"
+                  className="w-full"
+                  type="password"
+                  autoComplete="current-password"
+                  value={formData.password}
+                  onChange={(e) => handleChange("password", e.target.value)}
+                  error={!!passwordError}
+                  helperText={passwordError}
+                />
+              </div>
+              <div className="w-2/3 mx-auto">
+                <TextField
+                  id="outlined-basic"
+                  required
+                  label="Re-type Password"
+                  variant="outlined"
+                  className="w-full"
+                  type="password"
+                  autoComplete="current-password"
+                  value={formData.passwordConfirmation}
+                  onChange={(e) =>
+                    handleChange("passwordConfirmation", e.target.value)
+                  }
+                  error={!!passwordError}
+                  helperText={passwordError}
+                />
+              </div>
+              <div className="w-2/3 mx-auto">
+                <TextField
+                  id="outlined-basic"
+                  required
+                  label="Confirmation Code"
+                  variant="outlined"
+                  className="w-full"
+                  value={formData.confirmationCode}
+                  onChange={(e) =>
+                    handleChange("confirmationCode", e.target.value)
+                  }
+                />
+              </div>
+
+              <div className="flex flex-row mx-10 justify-center">
+                <div className="w-1/2 flex justify-center">
+                  <a href="login" className="text-blue-600 hover:text-blue-800">
+                    Sign In Instead
+                  </a>
+                </div>
+                <div className="w-1/2 flex justify-center">
+                  <Button
+                    variant="contained"
+                    type="submit"
+                    className="justify-end w-1/2 whitespace-nowrap"
+                    onClick={handleSubmit}
+                    disabled={!!passwordError}
+                  >
+                    Sign up
+                  </Button>
+                </div>
+              </div>
           </div>
         </div>
-      </div>
+      </div >
       <Footer />
     </>
   );
