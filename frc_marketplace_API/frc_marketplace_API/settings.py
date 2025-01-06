c22c3d50bde44d25621b24887596019c447f2041
ericg4 <eric.g4@outlook.com>
2025-01-06 15:15:48 -0800

    moved secret key to .env and added smtp traffic routing in default.conf

--- frc_marketplace_API/frc_marketplace_API/settings.py
+++ frc_marketplace_API/frc_marketplace_API/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-vaj&&)r(b638*t&z5l+0cuz*)dkh)3sslmwrzr!br&m^uel$mf"
+# Get SECRET_KEY from environment variable
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
