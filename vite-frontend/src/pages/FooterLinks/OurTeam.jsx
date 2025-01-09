5b046345804b5ce82bbabe1461b82df800a378d9
elem14 <lemkee106@gmail.com>
2025-01-09 10:56:15 -0800

    edited bio desc

--- vite-frontend/src/pages/FooterLinks/OurTeam.jsx
+++ vite-frontend/src/pages/FooterLinks/OurTeam.jsx
@@ -84,7 +84,7 @@ export default function OurTeamPage() {
             name={"Ethan Lemke"}
             image={"/ethanbutt.jpg"}
             title={"Fullstack Dev, 3647 Mentor"}
-            description={" geeked off the flizzy "}
+            description={"3647 alum also "}
             linkedin={"https://www.linkedin.com/in/ethan-lemke-492838254/"}
             insta={"https://www.instagram.com/_ethanlemke_/"}
           />
